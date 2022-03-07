899b811c5053afa748443ed333c5ccca52bfee30
Lin Yang <reaver@flomesh.io>
2022-03-07 16:13:07 +0800

    feat: change LICENSE from NEU to MIT
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- config/ingress-pipy/start.sh
+++ config/ingress-pipy/start.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
 
 #
-# The NEU License
+# MIT License
 #
-# Copyright (c) 2022.  flomesh.io
+# Copyright (c) 2022-2022.  flomesh.io
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do
-# so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# (1)The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# (2)If the software or part of the code will be directly used or used as a
-# component for commercial purposes, including but not limited to: public cloud
-#  services, hosting services, and/or commercial software, the logo as following
-#  shall be displayed in the eye-catching position of the introduction materials
-# of the relevant commercial services or products (such as website, product
-# publicity print), and the logo shall be linked or text marked with the
-# following URL.
-#
-# LOGO : http://flomesh.cn/assets/flomesh-logo.png
-# URL : https://github.com/flomesh-io
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
