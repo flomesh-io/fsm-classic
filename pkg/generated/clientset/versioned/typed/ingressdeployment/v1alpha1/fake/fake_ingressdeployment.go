8c87e158fa963aa8e05e4a9b664f334db6870000
Lin Yang <reaver@flomesh.io>
2022-07-18 15:56:03 +0800

    build(deps): upgrade go from 1.17 to 1.18

--- pkg/generated/clientset/versioned/typed/ingressdeployment/v1alpha1/fake/fake_ingressdeployment.go
+++ pkg/generated/clientset/versioned/typed/ingressdeployment/v1alpha1/fake/fake_ingressdeployment.go
@@ -124,7 +124,7 @@ func (c *FakeIngressDeployments) UpdateStatus(ctx context.Context, ingressDeploy
 // Delete takes name of the ingressDeployment and deletes it. Returns an error if one occurs.
 func (c *FakeIngressDeployments) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	_, err := c.Fake.
-		Invokes(testing.NewDeleteAction(ingressdeploymentsResource, c.ns, name), &v1alpha1.IngressDeployment{})
+		Invokes(testing.NewDeleteActionWithOptions(ingressdeploymentsResource, c.ns, name, opts), &v1alpha1.IngressDeployment{})
 
 	return err
 }
--- pkg/generated/clientset/versioned/typed/ingressdeployment/v1alpha1/ingressdeployment_client.go
+++ pkg/generated/clientset/versioned/typed/ingressdeployment/v1alpha1/ingressdeployment_client.go
@@ -26,6 +26,8 @@
 package v1alpha1
 
 import (
+	"net/http"
+
 	v1alpha1 "github.com/flomesh-io/fsm/apis/ingressdeployment/v1alpha1"
 	"github.com/flomesh-io/fsm/pkg/generated/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
@@ -46,12 +48,28 @@ func (c *IngressdeploymentV1alpha1Client) IngressDeployments(namespace string) I
 }
 
 // NewForConfig creates a new IngressdeploymentV1alpha1Client for the given config.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*IngressdeploymentV1alpha1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	client, err := rest.RESTClientFor(&config)
+	httpClient, err := rest.HTTPClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return NewForConfigAndClient(&config, httpClient)
+}
+
+// NewForConfigAndClient creates a new IngressdeploymentV1alpha1Client for the given config and http client.
+// Note the http client provided takes precedence over the configured transport values.
+func NewForConfigAndClient(c *rest.Config, h *http.Client) (*IngressdeploymentV1alpha1Client, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
 	}
