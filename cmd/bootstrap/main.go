c96a627531c7ec355c74225ed0a08dde38d4ac63
Lin Yang <reaver@flomesh.io>
2022-08-30 00:02:39 +0800

    fix: handle empty server names in case SNI is enabled (#90)
    * fix: handle empty server names in case SNI is enabled
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * fix: repackage pipy scripts
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * chore: add log for debugging
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * refactor: restructure mesh-config
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * fix: a typo
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * chore: bump chart version to v0.1.7
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    * fix: wrong assignment of modified content
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>
    
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- cmd/bootstrap/main.go
+++ cmd/bootstrap/main.go
@@ -82,17 +82,21 @@ func main() {
 	// 2. upload init scripts to pipy repo
 	repoClient := repo.NewRepoClientWithApiBaseUrl(mc.RepoApiBaseURL())
 	initRepo(repoClient)
-	if mc.Ingress.TLS {
-		if mc.Ingress.TLSOffload && mc.Ingress.SSLPassthrough.Enabled {
+	klog.V(5).Infof("mc.Ingress.TLS.Enabled=%t", mc.Ingress.TLS.Enabled)
+	if mc.Ingress.TLS.Enabled {
+		klog.V(5).Infof("mc.Ingress.TLS.TLSOffload.Enabled=%t", mc.Ingress.TLS.TLSOffload.Enabled)
+		klog.V(5).Infof("mc.Ingress.TLS.SSLPassthrough.Enabled=%t", mc.Ingress.TLS.SSLPassthrough.Enabled)
+
+		if mc.Ingress.TLS.TLSOffload.Enabled && mc.Ingress.TLS.SSLPassthrough.Enabled {
 			klog.Errorf("Both TLSOffload and SSLPassthrough are enabled, they are mutual exclusive, please check MeshConfig.")
 			os.Exit(1)
 		}
 
-		if mc.Ingress.TLSOffload {
+		if mc.Ingress.TLS.TLSOffload.Enabled {
 			issueCertForIngress(repoClient, certMgr, mc)
 		}
 
-		if mc.Ingress.SSLPassthrough.Enabled {
+		if mc.Ingress.TLS.SSLPassthrough.Enabled {
 			enableSSLPassthrough(repoClient, mc)
 		}
 	}
@@ -271,6 +275,7 @@ func issueCertForIngress(repoClient *repo.PipyRepoClient, certMgr certificate.Ma
 }
 
 func enableSSLPassthrough(repoClient *repo.PipyRepoClient, mc *config.MeshConfig) {
+	klog.V(5).Infof("SSL passthrough is enabled, updating repo config ...")
 	// 1. get main.json
 	path := "/base/ingress/config/main.json"
 	json, err := repoClient.GetFile(path)
@@ -280,12 +285,15 @@ func enableSSLPassthrough(repoClient *repo.PipyRepoClient, mc *config.MeshConfig
 	}
 
 	// 2. update ssl passthrough config
-	newJson, err := sjson.Set(json, "sslPassthrough.enabled", mc.Ingress.SSLPassthrough.Enabled)
+	klog.V(5).Infof("mc.Ingress.TLS.SSLPassthrough.Enabled=%t", mc.Ingress.TLS.SSLPassthrough.Enabled)
+	newJson, err := sjson.Set(json, "sslPassthrough.enabled", mc.Ingress.TLS.SSLPassthrough.Enabled)
 	if err != nil {
 		klog.Errorf("Failed to update sslPassthrough.enabled: %s", err)
 		os.Exit(1)
 	}
-	newJson, err = sjson.Set(json, "sslPassthrough.upstreamPort", mc.Ingress.SSLPassthrough.UpstreamPort)
+
+	klog.V(5).Infof("mc.Ingress.TLS.SSLPassthrough.UpstreamPort=%d", mc.Ingress.TLS.SSLPassthrough.UpstreamPort)
+	newJson, err = sjson.Set(newJson, "sslPassthrough.upstreamPort", mc.Ingress.TLS.SSLPassthrough.UpstreamPort)
 	if err != nil {
 		klog.Errorf("Failed to update sslPassthrough.upstreamPort: %s", err)
 		os.Exit(1)
