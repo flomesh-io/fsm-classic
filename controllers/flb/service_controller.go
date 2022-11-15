dff003885d12dc0543ad02133c9a2dc163568b37
Lin Yang <reaver@flomesh.io>
2022-11-15 23:47:10 +0800

    refactor: retry after 5 seconds if cannot obtain external IP
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- controllers/flb/service_controller.go
+++ controllers/flb/service_controller.go
@@ -247,7 +247,7 @@ func (r *ServiceReconciler) createOrUpdateFlbEntry(ctx context.Context, svc *cor
 	}
 
 	if len(resp.LBIPs) == 0 {
-		return ctrl.Result{}, fmt.Errorf("FLB hasn't assigned any external IP for service %s/%s", svc.Namespace, svc.Name)
+		return ctrl.Result{RequeueAfter: 5 * time.Second}, fmt.Errorf("FLB hasn't assigned any external IP for service %s/%s", svc.Namespace, svc.Name)
 	}
 
 	klog.V(5).Infof("External IPs assigned by FLB: %#v", resp)
