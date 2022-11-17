3aea320132e65b6052090387d9dade9460f197cb
Lin Yang <reaver@flomesh.io>
2022-11-17 10:42:59 +0800

    fix: mismatch type in json and struct
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- pkg/repo/types.go
+++ pkg/repo/types.go
@@ -25,7 +25,7 @@
 package repo
 
 type Codebase struct {
-	Version     int64    `json:"version,omitempty"`
+	Version     int64    `json:"version,string,omitempty"`
 	Path        string   `json:"path,omitempty"`
 	Main        string   `json:"main,omitempty"`
 	Base        string   `json:"base,omitempty"`
