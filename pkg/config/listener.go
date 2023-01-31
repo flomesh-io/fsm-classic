48a4f20c84693f15a62fee572f1320d911842dd6
Lin Yang <reaver@flomesh.io>
2023-01-31 22:37:27 +0800

    fix: mTLS config change should not trigger updating service spec
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- pkg/config/listener.go
+++ pkg/config/listener.go
@@ -168,22 +168,24 @@ func (l meshCfgChangeListenerForBasicConfig) OnConfigCreate(cfg *MeshConfig) {
 func (l meshCfgChangeListenerForBasicConfig) OnConfigUpdate(oldCfg, cfg *MeshConfig) {
 	klog.V(5).Infof("Updating basic config ...")
 
-	if cfg.Ingress.Enabled &&
-		(oldCfg.Ingress.HTTP.Enabled != cfg.Ingress.HTTP.Enabled ||
-			oldCfg.Ingress.HTTP.Listen != cfg.Ingress.HTTP.Listen) {
+	if isHTTPConfigChanged(oldCfg, cfg) {
 		if err := UpdateIngressHTTPConfig(commons.DefaultIngressBasePath, repo.NewRepoClient(cfg.RepoRootURL()), cfg); err != nil {
 			klog.Errorf("Failed to update HTTP config: %s", err)
 		}
 	}
 
-	if oldCfg.Ingress.TLS.Enabled != cfg.Ingress.TLS.Enabled ||
-		oldCfg.Ingress.TLS.Listen != cfg.Ingress.TLS.Listen ||
-		oldCfg.Ingress.TLS.MTLS != cfg.Ingress.TLS.MTLS {
+	if isTLSConfigChanged(oldCfg, cfg) {
 		if err := UpdateIngressTLSConfig(commons.DefaultIngressBasePath, repo.NewRepoClient(cfg.RepoRootURL()), cfg); err != nil {
 			klog.Errorf("Failed to update TLS config: %s", err)
 		}
 	}
 
+	if shouldUpdateIngressControllerServiceSpec(oldCfg, cfg) {
+		l.updateIngressControllerSpec(oldCfg, cfg)
+	}
+}
+
+func (l meshCfgChangeListenerForBasicConfig) updateIngressControllerSpec(oldCfg *MeshConfig, cfg *MeshConfig) {
 	selector := labels.SelectorFromSet(
 		map[string]string{
 			"app.kubernetes.io/component":   "controller",
@@ -196,14 +198,14 @@ func (l meshCfgChangeListenerForBasicConfig) OnConfigUpdate(oldCfg, cfg *MeshCon
 		List(context.TODO(), metav1.ListOptions{LabelSelector: selector.String()})
 
 	if err != nil {
-        klog.Errorf("Failed to list all ingress-pipy services: %s", err)
-        return
+		klog.Errorf("Failed to list all ingress-pipy services: %s", err)
+		return
 	}
 
 	// as container port of pod is informational, only change svc spec is enough
 	for _, svc := range svcList.Items {
 		service := svc.DeepCopy()
-		ports := make([]corev1.ServicePort, 0)
+		service.Spec.Ports = nil
 
 		if cfg.Ingress.HTTP.Enabled {
 			httpPort := corev1.ServicePort{
@@ -215,7 +217,7 @@ func (l meshCfgChangeListenerForBasicConfig) OnConfigUpdate(oldCfg, cfg *MeshCon
 			if cfg.Ingress.HTTP.NodePort > 0 {
 				httpPort.NodePort = cfg.Ingress.HTTP.NodePort
 			}
-			ports = append(ports, httpPort)
+			service.Spec.Ports = append(service.Spec.Ports, httpPort)
 		}
 
 		if cfg.Ingress.TLS.Enabled {
@@ -228,11 +230,10 @@ func (l meshCfgChangeListenerForBasicConfig) OnConfigUpdate(oldCfg, cfg *MeshCon
 			if cfg.Ingress.TLS.NodePort > 0 {
 				tlsPort.NodePort = cfg.Ingress.TLS.NodePort
 			}
-			ports = append(ports, tlsPort)
+			service.Spec.Ports = append(service.Spec.Ports, tlsPort)
 		}
 
-		if len(ports) > 0 {
-			service.Spec.Ports = ports
+		if len(service.Spec.Ports) > 0 {
 			if _, err := l.k8sApi.Client.CoreV1().
 				Services(GetFsmNamespace()).
 				Update(context.TODO(), service, metav1.UpdateOptions{}); err != nil {
@@ -242,6 +243,31 @@ func (l meshCfgChangeListenerForBasicConfig) OnConfigUpdate(oldCfg, cfg *MeshCon
 			klog.Warningf("Both HTTP and TLS are disabled, ignore updating ingress-pipy service")
 		}
 	}
+}
+
+func isHTTPConfigChanged(oldCfg *MeshConfig, cfg *MeshConfig) bool {
+	return cfg.Ingress.Enabled &&
+		(oldCfg.Ingress.HTTP.Enabled != cfg.Ingress.HTTP.Enabled ||
+			oldCfg.Ingress.HTTP.Listen != cfg.Ingress.HTTP.Listen)
+}
+
+func isTLSConfigChanged(oldCfg *MeshConfig, cfg *MeshConfig) bool {
+	return cfg.Ingress.Enabled &&
+		(oldCfg.Ingress.TLS.Enabled != cfg.Ingress.TLS.Enabled ||
+			oldCfg.Ingress.TLS.Listen != cfg.Ingress.TLS.Listen ||
+			oldCfg.Ingress.TLS.MTLS != cfg.Ingress.TLS.MTLS)
+}
+
+func shouldUpdateIngressControllerServiceSpec(oldCfg, cfg *MeshConfig) bool {
+	return cfg.Ingress.Enabled &&
+		(oldCfg.Ingress.TLS.Enabled != cfg.Ingress.TLS.Enabled ||
+			oldCfg.Ingress.TLS.Listen != cfg.Ingress.TLS.Listen ||
+			oldCfg.Ingress.TLS.Bind != cfg.Ingress.TLS.Bind ||
+			oldCfg.Ingress.TLS.NodePort != cfg.Ingress.TLS.NodePort ||
+			oldCfg.Ingress.HTTP.Enabled != cfg.Ingress.HTTP.Enabled ||
+			oldCfg.Ingress.HTTP.Listen != cfg.Ingress.HTTP.Listen ||
+			oldCfg.Ingress.HTTP.NodePort != cfg.Ingress.HTTP.NodePort ||
+			oldCfg.Ingress.HTTP.Bind != cfg.Ingress.HTTP.Bind)
 }
 
 func (l meshCfgChangeListenerForBasicConfig) OnConfigDelete(cfg *MeshConfig) {
