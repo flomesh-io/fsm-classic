96a5d6c54e2457540658bb7204ae5df7060d3698
Lin Yang <reaver@flomesh.io>
2023-04-06 10:38:45 +0800

    chore: [skip ci] add TODO
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- controllers/gateway/v1beta1/gateway_controller.go
+++ controllers/gateway/v1beta1/gateway_controller.go
@@ -196,6 +196,9 @@ func (r *gatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		activeGateways[gateway.Namespace] = activeGateway
 	}
 
+    // TODO: implement it
+    // 6. update addresses of Gateway status if any IP is allocated
+
 	return ctrl.Result{}, nil
 }
 
